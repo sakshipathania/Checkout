c034e218d247a719de8a257a9580aabb5df28cb8
sakshipathania <62547853+sakshipathania@users.noreply.github.com>
2022-05-05 21:59:02 +0530

    Update Stripe_checkout.java

--- src/test/java/SetupClass/TestStep/Stripe_checkout.java
+++ src/test/java/SetupClass/TestStep/Stripe_checkout.java
@@ -730,7 +730,11 @@ public class Stripe_checkout extends Set{
 		delete_profile_coupon.click();
 		 Thread.sleep(30000);
 		
-
+                String verifyDeleteAccount = wait
+				.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@data-bind='html: $parent.prepareMessageForHtml(message.text)']"))).getText();
+		 Thread.sleep(3000);
+	         Assert.assertTrue("Account is not deleted", verifyDeleteAccount.contains("Your account has been deleted successfully."));
+	         System.out.println("your account delete successfully");
 	}
 	
 }
